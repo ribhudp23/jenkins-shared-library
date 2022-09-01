0323254a71d9c19e80651289169b5cf650f68921
ribhudp23 <108716875+ribhudp23@users.noreply.github.com>
2022-09-01 15:09:44 +0530

    Try using license server instead of mac address

--- vars/getDockerConfig.groovy
+++ vars/getDockerConfig.groovy
@@ -25,7 +25,7 @@ def call(java.util.ArrayList listOfResources, matlabHSPro=true, UseNFS=false) {
         }
         // Add correct MAC to licenses work in Docker
         withCredentials([string(credentialsId: 'MAC_ADDR', variable: 'MAC_ADDR')]) {
-            args.add('--mac-address ' + MAC_ADDR)
+            args.add('--LICENSE_SERVER ' + MAC_ADDR)
         }
         }
         else if (listOfResources[i].equalsIgnoreCase( 'Vivado' )) {
